574dc66eff5adabe7046341e0e9661f7ed7c292c
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-25 13:57:11 -0400

    changed sequelize sync to false

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 //turns connection on to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App now listening on port ${PORT}`))
 })
