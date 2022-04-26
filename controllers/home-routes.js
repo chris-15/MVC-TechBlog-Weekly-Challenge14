5cc5ab2e0e617271053e6e32a22d5c2a35796182
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-26 16:14:01 -0400

    added the single post view and ability to add comments on the page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,8 +22,8 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-        // will loop over and map each sequalize object into serialized version of itself, into new array
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // will loop over and map each sequalize object into serialized version of itself, into new array
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
       // pass a single post object into the homepage template
       res.render("homepage", { posts });
     })
@@ -34,12 +34,53 @@ router.get("/", (req, res) => {
 });
 
 // route to display log in page
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // if already logged in it will send you back to homepage
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
-  res.render('login');
-})
+  res.render("login");
+});
+
+// route to display a single post
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "post_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", { post });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
