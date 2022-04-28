1fb8ef928bcd947d667173f4a4abf5e4e0e35949
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-28 13:13:09 -0400

    added dahsboard functionailty and fixed null values on user id

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // if bad request send error 404 letting user know it is bad request
 router.use((req, res) => {
